6d6f28f159032b251c11682323df19797510912e
Remus_Jones <remus.jones@hotmail.com>
2020-02-13 20:41:07 +1100

    Fixed plugin.yml & added welcome message.
    Fixed indentations on Plugin.yml causing issues.
    Implemented basic welcome to server listener class to welcome players to server.
    Changed logging level to INFO.
    Added MC server testing environment, has a stripped version of the server (not including world).

--- Riverland_dev/src/main/java/riverland/dev/riverland/Riverland.java
+++ Riverland_dev/src/main/java/riverland/dev/riverland/Riverland.java
@@ -1,24 +1,35 @@
 package riverland.dev.riverland;
-
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.logging.Level;
 
 public final class Riverland extends JavaPlugin {
 
+
+    FileConfiguration config = getConfig();
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        config.addDefault("WelcomeMessage", "Welcome to Riverlands!");
+        config.options().copyDefaults(true);
+        saveConfig();
 
-        getLogger().log(Level.FINE,"Riverland Plugin Enabled");
-        // register Foo..
+        getLogger().log(Level.INFO,"Riverland Plugin Enabled");
+        // register command against plugin.yml commands list..
         this.getCommand("foo").setExecutor(new Foo());
+        // register listener..
+
+        String welcomeMsg = config.getString("WelcomeMessage");
+        PlayerJoinAutoMessage welcomeMessage = new PlayerJoinAutoMessage(welcomeMsg,this);
+        getServer().getPluginManager().registerEvents(welcomeMessage, this);
+
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        getLogger().log(Level.FINE,"Riverland Plugin Disabled");
+        getLogger().log(Level.INFO,"Riverland Plugin Disabled");
     }
 
 }
